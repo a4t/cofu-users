c06da0a3698cd01b3a460d51874c571948d3265c
a4t <iwanomoto@gmail.com>
2016-12-08 11:44:06 +0900

    jsonファイル別に実行するように変更

--- recipe.lua
+++ recipe.lua
@@ -1,55 +1,53 @@
-for username, item in pairs(var.users) do
-  user (username) {
-    uid = item.uid,
-    password = item.password,
-    shell = item.shell,
-    create_home = true,
-  }
+user (var.name) {
+  uid = var.uid,
+  password = var.password,
+  shell = var.shell,
+  create_home = true,
+}
 
-  directory ("/home/"..username.."/.ssh") {
-    action = "create",
-    mode = "0700",
-    owner = username,
-    group = username,
-  }
+directory ("/home/"..var.name.."/.ssh") {
+  action = "create",
+  mode = "0700",
+  owner = var.name,
+  group = var.name,
+}
 
-  file ("/home/"..username.."/.ssh/config") {
-    owner = username,
-    group = username,
-    mode = '644',
-    content = [=[
+file ("/home/"..var.name.."/.ssh/config") {
+  owner = var.name,
+  group = var.name,
+  mode = '644',
+  content = [=[
 ForwardAgent yes
 host github.com
-    StrictHostusernameChecking no
+    StrictHostuservar.nameChecking no
 ]=]
-  }
+}
 
-  execute "reset public usernames" {
-    command = ":> /home/"..username.."/.ssh/authorized_keys",
-  }
+execute "reset public uservar.names" {
+  command = ":> /home/"..var.name.."/.ssh/authorized_keys",
+}
 
-  for num, public_key in pairs(item.public_keys) do
-    execute 'set public key' do
-      command = "echo "..public_key.." >> /home/"..username.."/.ssh/authorized_keys"
-    end
+for num, public_key in pairs(var.public_keys) do
+  execute 'set public key' do
+    command = "echo "..public_key.." >> /home/"..var.name.."/.ssh/authorized_keys"
   end
+end
 
-  file ("/home/"..username.."/.ssh/authorized_keys") {
-    owner = username,
-    group = username,
-    mode = '600',
+file ("/home/"..var.name.."/.ssh/authorized_keys") {
+  owner = var.name,
+  group = var.name,
+  mode = '600',
+}
+
+if var.wheel then
+  file ("/etc/sudoers.d/"..var.name) {
+    owner = 'root',
+    group = 'root',
+    mode = '644',
+    content = var.name.." ALL=(ALL) ALL"
   }
-
-  if item.wheel then
-    file ("/etc/sudoers.d/"..username) {
-      owner = 'root',
-      group = 'root',
-      mode = '644',
-      content = username.." ALL=(ALL) ALL"
-    }
-  else
-    file ("/etc/sudoers.d/"..username) {
-      action = 'delete'
-    }
-  end
+else
+  file ("/etc/sudoers.d/"..var.name) {
+    action = 'delete'
+  }
 end
